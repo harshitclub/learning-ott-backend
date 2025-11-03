549b844f838dbe3dc3d737a0ff263e702cd0d8af
harshitclub <harshitclub@gmail.com>
2025-11-03 16:33:31 +0530

    feat(zod): add zod validators

---
+++ src/validators/ZodErrorMessages.ts
@@ -0,0 +1,20 @@
+export enum ZodErrorMessages {
+  // Name
+  FIRST_NAME_MIN = 'First name must be at least 2 characters long',
+  FIRST_NAME_MAX = 'First name is too long',
+  LAST_NAME_MIN = 'Last name must be at least 2 characters long',
+  LAST_NAME_MAX = 'Last name is too long',
+
+  // Email
+  EMAIL_INVALID = 'Invalid email format',
+  EMAIL_MAX = 'Email is too long',
+
+  // Phone
+  PHONE_MIN = 'Phone number must be at least 10 digits',
+  PHONE_MAX = 'Phone number cannot exceed 15 digits',
+
+  // Password
+  PASSWORD_REQUIRED = 'Password is required',
+  PASSWORD_MIN = 'Password must be at least 6 characters',
+  PASSWORD_COMPLEXITY = 'Password must be at least 6 characters long, include 1 uppercase letter, 1 number, and 1 special character (@$!%*?&)'
+}
