9bc9983bd732e22f3f15aa22ec9cfa53abce595b
harshitclub <harshitclub@gmail.com>
2025-11-04 12:12:53 +0530

    chore: adding further complete changes

--- src/routes/v1/user.routes.ts
+++ src/routes/v1/user.routes.ts
@@ -5,12 +5,13 @@ import {
   userSignup,
   userUpdate
 } from '../../controllers/user.controllers'
+import { asyncHandler } from '../../middlewares/asyncHandler'
 
 const userRoutesV1 = express.Router()
 
-userRoutesV1.post('/signup', userSignup)
-userRoutesV1.post('/login', userLogin)
-userRoutesV1.get('/me', userProfile)
-userRoutesV1.patch('/me', userUpdate)
+userRoutesV1.post('/signup', asyncHandler(userSignup))
+userRoutesV1.post('/login', asyncHandler(userLogin))
+userRoutesV1.get('/me', asyncHandler(userProfile))
+userRoutesV1.patch('/me', asyncHandler(userUpdate))
 
 export default userRoutesV1
